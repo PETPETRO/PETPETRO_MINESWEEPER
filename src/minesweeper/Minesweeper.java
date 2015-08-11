92f90c64e2139cedf9401c526263a18d48a0eb33
PETPETRO <peterpetrovaj@gmail.com>
2015-08-11 07:38:21 +0200

    Task 7

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,5 +1,7 @@
 package minesweeper;
 
+import java.io.IOException;
+
 import minesweeper.consoleui.ConsoleUI;
 import minesweeper.core.Field;
 
@@ -10,15 +12,32 @@ public class Minesweeper {
 	/** User interface. */
 
 	private UserInterface userInterface;
+	private long startMillis;
+	private static Minesweeper instance;
+	private BestTimes bestTimes;
+	private Settings setting;
 
 	/**
 	 * Constructor.
+	 * 
+	 * @throws IOException
+	 * @throws ClassNotFoundException
 	 */
-	private Minesweeper() {
+	private Minesweeper() throws ClassNotFoundException, IOException {
+		instance = this;
 		userInterface = new ConsoleUI();
+		bestTimes = new BestTimes();
+		setting = Settings.load();
+		setSetting(setting);
 
-		Field field = new Field(10, 10, 10);
+		Field field = new Field(setting.getRowCount(), setting.getColumnCount(), setting.getMineCount());
+		bestTimes.addPlayerTime("Fero", 145);
+		bestTimes.addPlayerTime("Jano", 45);
+		bestTimes.addPlayerTime("Mato", 78);
+		bestTimes.addPlayerTime("Vlado", 3);
+		System.out.println(bestTimes.toString());
 
+		startMillis = System.currentTimeMillis();
 		userInterface.newGameStarted(field);
 
 	}
@@ -28,10 +47,35 @@ public class Minesweeper {
 	 * 
 	 * @param args
 	 *            arguments
+	 * @throws IOException
+	 * @throws ClassNotFoundException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
 
 		new Minesweeper();
 
 	}
+
+	public int getPlayingSeconds() {
+		return (int) ((System.currentTimeMillis() - startMillis) / 1000);
+	}
+
+	public static Minesweeper getInstance() {
+		return instance;
+
+	}
+
+	public BestTimes getBestTimes() {
+		return bestTimes;
+	}
+
+	public Settings getSetting() {
+		return setting;
+	}
+
+	public void setSetting(Settings setting) throws IOException {
+		this.setting = setting;
+		setting.save();
+	}
+
 }
--- src/minesweeper/core/Field.java
+++ src/minesweeper/core/Field.java
@@ -44,11 +44,25 @@ public class Field {
 	 *            mine count
 	 */
 	public Field(int rowCount, int columnCount, int mineCount) {
+
+		if (rowCount < 0) {
+			System.err.println("Pocet riadkov nemoze byt mensi ako nula!");
+			System.exit(0);
+
+		} else if (columnCount < 0) {
+			System.err.println("Pocet stpcov nemoze byt mensi ako nula!");
+			System.exit(0);
+		} else if (mineCount > rowCount * columnCount || mineCount < 0) {
+			System.err.println("Pocet min musi byt vacsi ako nula a mensi alebo rovny poctu dlazdic!");
+			System.exit(0);
+		}
+
 		this.rowCount = rowCount;
 		this.columnCount = columnCount;
 		this.mineCount = mineCount;
 		tiles = new Tile[rowCount][columnCount];
 		generate();
+
 	}
 
 	/**
@@ -73,7 +87,7 @@ public class Field {
 				return;
 			}
 
-			if (tile instanceof Clue && ((Clue) tile).getValue() == 0) {
+			if (((Clue) tile).getValue() == 0) {
 				openAdjacentTiles(row, column);
 			}
 		}
@@ -101,6 +115,7 @@ public class Field {
 	 * Generates playing field.
 	 */
 	private void generate() {
+
 		for (int c = 0; c < mineCount; c++) {
 			int row = new Random().nextInt(rowCount);
 			int column = new Random().nextInt(columnCount);
@@ -119,6 +134,7 @@ public class Field {
 			}
 
 		}
+
 	}
 
 	/**
@@ -157,6 +173,10 @@ public class Field {
 		}
 
 		return count;
+	}
+
+	public int getRemainingMineCount() {
+		return mineCount - getNumberOf(State.MARKED);
 	}
 
 	/**
